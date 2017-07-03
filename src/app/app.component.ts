e4e0066f5a8d21c85ef827e42738adf52df5f1ea
Johnny IV Young O <asesordeprogramacion@gmail.com>
2017-07-02 23:30:04 -0500

    support sort items descending

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,4 +23,8 @@ export class AppComponent {
     link.value = '';
     return false;
   }
+
+  shortArticle(): Article[] {
+    return this.articles.sort( (a: Article, b: Article) => b.votes - a.votes );
+  }
 }
